c5e03e56b2c7dd5dca914600be3c781556d8149e
Mark Shamanskii <darkweid713@gmail.com>
2024-11-19 13:31:53 +0500

    Docs: added docstring

--- task1/solution.py
+++ task1/solution.py
@@ -2,6 +2,20 @@ from functools import wraps
 
 
 def strict(func):
+    """
+    A decorator that checks if the types of arguments passed to a function
+    match the types declared in the function's type annotations.
+
+    Args:
+        func (function): The function to be decorated.
+
+    Returns:
+        function: A wrapper function that performs type checking before calling the original function.
+
+    Raises:
+        TypeError: If any argument type does not match the declared type in the function's annotations.
+    """
+
     @wraps(func)
     def wrapper(*args):
         types_of_args = map(type, args)
